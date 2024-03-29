2cfa33cb39be331cfd72984b4b0c146e277caac4
Jason Xu <97411709+JasonXu0920@users.noreply.github.com>
2024-03-29 12:25:38 +1100

    create campaign function

--- web3/contracts/SmartCap.sol
+++ web3/contracts/SmartCap.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.8.9;
 
 contract SmartCap {
     struct Campaign {
-        address onwer;
+        address owner;
         string title;
         string description;
         uint256 target;
-        uint256 deadlin;
+        uint256 deadline;
         uint256 amountCollected;
         string image;
         address[] donators;
@@ -15,6 +15,38 @@ contract SmartCap {
     }
 
     mapping(uint256 => Campaign) public campaigns;
-    
+
     uint256 public numberOfCampaigns = 0;
+
+    function createCampaign(
+        address _owner,
+        string memory _title,
+        string memory _description,
+        uint256 _target,
+        uint256 _deadline,
+        string memory _image
+    ) public returns (uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
+        // is everything ok?
+        require(campaign.deadline < block.timestamp, "The deadline should be a date in the future.");
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1;
+    }
+
+    function donateToCampaign() {}
+
+    function getDonators() {}
+
+    function getCampaigns() {}
 }
